46a252fe79db255815a1821f840b336e40a2d4d9
emanuelhardwell <emanuelnoe16@gmail.com>
2025-02-24 22:22:54 -0600

    feat: Update project and user Entities

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,5 +1,6 @@
+import { BaseEntity } from '../../config/base.entity';
 import { IProject } from '../../interfaces/project.interface';
-import { BaseEntity, Column, Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity({ name: 'projects' })
 export class ProjectEntity extends BaseEntity implements IProject {
