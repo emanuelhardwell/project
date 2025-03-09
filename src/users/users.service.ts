57e3e18b632082a28e8f12593ae66f72a5e180be
emanuelhardwell <emanuelnoe16@gmail.com>
2025-03-08 19:37:25 -0600

    feat: Create method createUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HandleError } from '../common/errors/handle-error';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    private readonly handleError: HandleError,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create(createUserDto);
+      const userSaved = await this.userRepository.save(user);
+      delete userSaved.password;
+
+      return userSaved;
+    } catch (error) {
+      this.handleError.error(error);
+    }
   }
 
   findAll() {
