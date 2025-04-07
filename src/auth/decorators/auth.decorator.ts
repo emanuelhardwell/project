1c43be1f2a73470fa2a48440ba1e30675970615d
emanuelhardwell <emanuelnoe16@gmail.com>
2025-04-06 23:40:45 -0600

    feat: Apply decorator AccessLevelGuard

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -3,10 +3,11 @@ import { ROLES } from '../../enums/roles.enum';
 import { RoleProtected } from './role-protected.decorator';
 import { AuthGuard } from '@nestjs/passport';
 import { UserRoleGuard } from '../guards/user-role.guard';
+import { AccessLevelGuard } from '../guards/access-level.guard';
 
 export const Auth = (...roles: ROLES[]) => {
   return applyDecorators(
     RoleProtected(...roles),
-    UseGuards(AuthGuard(), UserRoleGuard),
+    UseGuards(AuthGuard(), UserRoleGuard, AccessLevelGuard),
   );
 };
