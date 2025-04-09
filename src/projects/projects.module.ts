b034e7ed477eda5e07d26238d12a087392414c26
juanklar <74260833+juanklar@users.noreply.github.com>
2025-04-08 22:18:28 -0600

    Revert "Develop to QA"

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -4,16 +4,9 @@ import { ProjectsController } from './projects.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectEntity } from './entities/project.entity';
 import { CommonModule } from '../common/common.module';
-import { AuthModule } from '../auth/auth.module';
-import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([ProjectEntity]),
-    CommonModule,
-    AuthModule,
-    UsersModule,
-  ],
+  imports: [TypeOrmModule.forFeature([ProjectEntity]), CommonModule],
   controllers: [ProjectsController],
   providers: [ProjectsService],
 })
